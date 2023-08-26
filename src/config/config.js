dfef00cfd7df4b7c9569ce763ad5489aaaaf1429
Abdelmounaim Oulaid <97753179+AbdelmounaimOulaid@users.noreply.github.com>
2023-08-26 14:42:26 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,4 @@ export const localUrl = 'https://vldo.shop/'
 export const serverUrl = 'https://api.vldo.shop/'
 
 
-// export const localUrl = 'http://localhost:8080/'
-// export const serverUrl = 'http://localhost:8000/'
-
 export const currency = "$";
